3b38ede806ea13768210b1e78e5640e8cb082c61
Pietro1-code <pietro.correa.silva@escola.pr.gov.br>
2025-07-04 10:33:12 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,51 @@
 criaCartao(
+    'Geografia',
+    'Qual o significado da sigla ONU?',
+    'significa: Organização das Nações Unidas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos cromossomos tem um ser humano?',
+    'O ser humano possui 46 cromossomos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a tradução da palavra beaultiful? ',
+    'significa linda/o em português'
+)
+criaCartao(
     'Programação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Química ',
+    'Qual o significado do elemento Ba?',
+    'Signifca Bário '
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Hitória',
+    'Quando acabou a segunda guerra mundial?',
+    'Ela acabou em 1945'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Conhecimentos gerais',
+    'Quem criou o facebook?',
+    'Foi Mark Zuckerberg'
+)
+criaCartao(
+    'Geografia ',
+    'Qual o significado da sigla OTAN',
+    'Ela significa Organização do Tratado do Atlântico Norte.'
 )
